63bae24829585fd96fecfb7a4a50c36b288e614e
Denis Fedoseev <denis.fedoseev@gmail.com>
2017-02-08 23:10:05 +0300

    Low Pass Filter was added

--- Tenso_sensor.ino
+++ Tenso_sensor.ino
@@ -1,43 +1,65 @@
 #include <Q2HX711.h>
 
+const bool DEBUG = false;
+
 const byte hx711_data_pin = A2;
 const byte hx711_clock_pin = A3;
 const int probe_pin = 7;
-const bool DEBUG = false;
 
+long trigger = 13000; //Weight difference trigger. Lower is better
 
-long trigger = 12000; //Weight difference trigger. Lower is better
-int tolerance = 3000; //
 long weight, prev_weight, trigger_weight = 0;
 bool is_pressed = false;
 
+
+
+//Filter parameters
+float FK = 0.9;
+int tolerance = 3000; //
+int values_count = 10;
 
 long timest = millis();
 
 Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
 
+
+//Get filtered values from HX711
+long getSensorValue() { //
+    long acc_x_raw, acc_x, acc_xf;
+    
+    for (int i = 0; i < values_count; i++) {
+       acc_x_raw = hx711.read();
+       acc_x = acc_x_raw - tolerance/1.0;
+       acc_xf = (1-FK)*acc_xf + FK*acc_x; 
+       delay(5);
+    }
+    
+    return acc_xf;
+}
+
 void setup() {
   pinMode(probe_pin, OUTPUT);
   digitalWrite(probe_pin, LOW);
   
-  Serial.begin(9600);
-  delay(3000);
+  Serial.begin(115200);
+  delay(1000);
 
-  prev_weight = weight = hx711.read();
+  prev_weight = weight = getSensorValue();
   Serial.println(prev_weight);
 }
 
 void loop() {
-  weight = hx711.read();
+  weight = getSensorValue();
+  
   if (DEBUG) {
     if (timest+500 <= millis()) {
-      Serial.println(weight);
+      char buffer [50];
+      sprintf (buffer, "current weight! [%li] [%li]", weight, prev_weight);
+      Serial.println(buffer);
       timest = millis();    
     }  
   }
   
-
-
   if (is_pressed) {
     if (DEBUG) {
        char buffer [50];
@@ -46,7 +68,7 @@ void loop() {
        Serial.println(buffer);  
     }
     
-    if ((trigger_weight + tolerance) >= weight) {
+    if (trigger_weight >= weight) {
       if (DEBUG) {
         char buffer [50];
         sprintf (buffer, "Trigger released! [%li]", weight);
@@ -56,17 +78,18 @@ void loop() {
       digitalWrite(probe_pin, LOW);
       is_pressed = false;
     } 
-  } else {
+  } else { 
     if ((weight - prev_weight) >= trigger) {
-      if (DEBUG) {
-        char buffer [50];
-        sprintf (buffer, "Trigger fired! [%li]", weight);
-        Serial.println(buffer);  
-      }
+        if (DEBUG) {
+          char buffer [50];
+          sprintf (buffer, "Trigger fired! [%li]", weight);
+          Serial.println(buffer);  
+        }
       
-      digitalWrite(probe_pin, HIGH);
-      is_pressed = true;
-      trigger_weight = weight;
+        digitalWrite(probe_pin, HIGH);
+        is_pressed = true;
+        trigger_weight = weight;
+      
     }
     
     prev_weight = weight;  
